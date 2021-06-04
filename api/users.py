0d1bc5ac707162bacc08df7d3c11e566f3c4d245
Alexey <lucker1st@gmail.com>
2021-06-04 22:09:01 +0500

    Intershipv2| Fixed the names of functions and variables

--- api/users.py
+++ api/users.py
@@ -15,37 +15,43 @@ with open("users_data.json", "r") as file:
     data_users = json.load(file)
 
 
-def find_user(key: str, value: str):
+def get_user_from_username(
+        data_filter: str = None,
+        username: str = None) \
+        -> list:
+    """ Function for search users by username in data.
+    :param data_filter: Where in data we will search username.
+    :param username: Username that we want to find in data.
+    :return: list of users with "username" inside of them.
     """
-    Function for search users
-    :param key:
-    :param value:
-    :return: needed users
-    """
-    logger.info(f"function 'return_value' get {key}")
+    logger.info(f"function 'return_value' get {data_filter}")
     # Getting all list of users
-    value_list = [item[key] for item in data_users["users"] if key in item.keys()]
+    value_list = [item[data_filter] for item in data_users["users"] if data_filter in item.keys()]
     # Find in all users value
-    answer = list(set(filter(lambda x: value.lower() in x.lower(), value_list)))
+    answer = list(set(filter(lambda user: username.lower() in user.lower(), value_list)))
     return answer
 
 
-def find_user_not_from_username(find: str, refund: str, value: str):
-    """
-    Function for search users from any filter
-    :param find: What parameters we want getting
-    :param refund: What parameters we use for searching users
-    :param value: searching value
-    :return: needed users from some filter
-    """
-    logger.info(f"function 'return_another_value' get {find, refund}")
+def get_user_from_other_param(
+        return_param: str,
+        choice_param: str,
+        value_choice_param: str) \
+        -> list:
+    """ Function for search users by other parameter in data
+        :param return_param: What parameters we want getting
+        :param choice_param: What parameters we use for searching users
+        :param value_choice_param: searching value
+
+        :return: List of users with needed department
+        """
+    logger.info(f"function 'return_another_value' get {return_param, choice_param}")
     # Getting list of users and list of departments
-    answer_1 = [item[find] for item in data_users["users"] if find in item.keys()]
-    answer_2 = [item[refund] for item in data_users["users"] if refund in item.keys()]
+    list_of_users = [item[return_param] for item in data_users["users"] if return_param in item.keys()]
+    list_of_departments = [item[choice_param] for item in data_users["users"] if choice_param in item.keys()]
     # getting dictionary with pair user:department
-    dictionary_values = dict(zip(answer_1, answer_2))
+    dictionary_values = dict(zip(list_of_users, list_of_departments))
     # Find needed users
-    answer = [item for item in dictionary_values.keys() if value.lower() in dictionary_values[item].lower()]
+    answer = [item for item in dictionary_values.keys() if value_choice_param.lower() in dictionary_values[item].lower()]
     logger.info(f"function 'return_another_value' return {dictionary_values}")
     return answer
 
@@ -57,22 +63,21 @@ def create_api():
     def users_name():
         user = request.args.get("username")
         department = request.args.get("department")
-
         logger.info(f"function 'users_name' get username = {user}, department = {department}")
         # Searching in database
-        if user is not None:
-            return jsonify(find_user("username", user))
-        if department is not None:
-            return jsonify(find_user_not_from_username("username", "department", department))
+        if user:
+            return jsonify(get_user_from_username("username", user))
+        if department:
+            return jsonify(get_user_from_other_param("username", "department", department))
         else:
             return jsonify(data_users)
 
-    @app_api.route('/department', methods=['GET'])
+    @app_api.route('/departments', methods=['GET'])
     def departs_name():
-        name = request.args.get("name")
-        logger.info(f"function 'departs_name' get name = {name}")
-        if name is not None:
-            return jsonify(find_user("department", name))
+        departament_name = request.args.get("name")
+        logger.info(f"function 'departs_name' get name = {departament_name}")
+        if departament_name:
+            return jsonify(get_user_from_username("department", departament_name))
         else:
             return jsonify(list(set(item["department"] for item in data_users["users"] if "department" in item.keys())))
 
